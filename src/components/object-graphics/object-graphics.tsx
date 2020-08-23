0b7c8221244c34b5e2032813b61513bb4942cefb
Krzysztof Kabat <othreumaru@gmail.com>
2020-08-23 14:19:38 +0100

    added puddle

--- src/components/object-graphics/object-graphics.tsx
+++ src/components/object-graphics/object-graphics.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Sprite, Container } from 'react-pixi-fiber';
 import * as PIXI from 'pixi.js';
 import bearTrapGraphics from '../../assets/img/bear_trap.png';
+import kaluzaGraphics from '../../assets/img/kaluza.png';
 
 interface Props {
   x?: number;
@@ -12,15 +13,16 @@ interface Props {
 const ANCHOR_POINT = new PIXI.Point(0, 0.5);
 const mipmapOption = { mipmap: PIXI.MIPMAP_MODES.ON };
 
-export const ObjectGraphics: React.FC<Props> = ({ x = 0, y = 0 }) => {
+const nameIdToTexture: any = {
+  ['bear-trap']: PIXI.Texture.from(bearTrapGraphics, mipmapOption),
+  ['puddle']: PIXI.Texture.from(kaluzaGraphics, mipmapOption),
+};
+
+export const ObjectGraphics: React.FC<Props> = ({ x = 0, y = 0, nameId }) => {
+  const tex = nameIdToTexture[nameId] || nameIdToTexture.puddle;
   return (
     <Container x={x} y={y}>
-      <Sprite
-        texture={PIXI.Texture.from(bearTrapGraphics, mipmapOption)}
-        width={266 / 4}
-        height={224 / 4}
-        anchor={ANCHOR_POINT}
-      />
+      <Sprite texture={tex} width={tex.width / 4} height={tex.height / 4} anchor={ANCHOR_POINT} />
     </Container>
   );
 };
